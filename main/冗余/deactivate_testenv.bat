6c9549e6d4330e25418476325f84646bdbab4e0c
帝皇的祈愿 <1517550296@qq.com>
2026-03-06 20:25:20 +0800

    更新.gitignore，只跟踪main文件夹内的内容

--- main/冗余/deactivate_testenv.bat
+++
@@ -1,5 +0,0 @@
-
-@echo off
-call test_env\Scripts\deactivate.bat
-echo 已退出 test_env 虚拟环境
-pause
--- main/冗余/run_test_in_testenv.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-cd /d "g:\gpt-sovits\v2\agent\main\plugins\RealtimeSTT-master"
-start cmd /k "call test_env\Scripts\activate.bat && python g:\gpt-sovits\v2\agent\main\test_realtimestt.py && pause"
